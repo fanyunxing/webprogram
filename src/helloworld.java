9fe1ad467ead8eefafd5af4a9b074b0dec43702c
fanyunxing <1516778301@qq.com>
2018-03-07 14:21:29 +0800

    create a new greeting file

--- src/helloworld.java
+++ src/helloworld.java
@@ -3,8 +3,11 @@
  * @date: 2018/3/7
  * @description:
  */
+import testpackage.greeting;
 public class helloworld {
     public static void main( String args[] ){
         System.out.println("HelloWorld!");
+        greeting a = new greeting();
+        a.greeting();
     }
 }
---
+++ src/testpackage/greeting.java
@@ -0,0 +1,12 @@
+package testpackage;
+
+/**
+ * @author: lyh
+ * @date: 2018/3/7
+ * @description:
+ */
+public class greeting {
+    public void greeting(){
+        System.out.println("Welcome Home!");
+    }
+}
